cf46c8f8679a69fbead1ed373c1b7abaf1bc451d
Yiting Lin <bestfriend830@gmail.com>
2021-10-02 16:05:05 +0800

    while i > 0

--- password.py
+++ password.py
@@ -1,6 +1,7 @@
 password = 'a123456'
 
-
+"""
+#v1.
 i = 1
 while True:
 	usertest = input('請輸入密碼： ')
@@ -12,3 +13,18 @@ while True:
 		if i == 3:
 			break
 	i = i + 1		
+"""
+
+
+#v2.
+i = 3
+while i > 0:
+	usertest = input('請輸入密碼： ')
+	if usertest == password:
+		print('登入成功！')
+		break
+	elif usertest != password:
+		i = i -1
+		print('密碼錯誤！還有', i, '次機會')
+
+
